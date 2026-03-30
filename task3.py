e2bc131bc112ec6a5c9cdff2502ab06e355f0ad3
Raven198628 <hachaturova.vv@edu.spbstu.ru>
2026-03-30 20:54:20 +0300

    Delete task3.py

--- task3.py
+++
@@ -1,7 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-middleIndex = len(list_players) // 2
-firstTeam = list_players[:middleIndex]
-secondTeam = list_players[middleIndex:]
-print(firstTeam)
-print(secondTeam)
